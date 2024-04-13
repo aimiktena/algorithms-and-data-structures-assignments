5c43a16e2502b8a86f1be5eaa5bb730657ea412c
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-04-13 18:52:04 +0300

    Function for calculating the center of the rectangle

---
+++ assignment-2024-1/tromino_tiling.py
@@ -0,0 +1,11 @@
+class TrominoTiling:
+
+    def calculateCenter (startX, endX, startY, endY):
+        centerX = int((startX + endX) / 2)
+        centerY = int((startY + endY) / 2)
+        return centerX, centerY
+    
+    
+
+
+
