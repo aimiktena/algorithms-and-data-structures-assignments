3549b34b6cae3539ca685a477887c7e584728324
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-04-17 14:22:16 +0300

    Add list of tromino indexes to enumerate the number of trominos that are created

--- assignment-2024-1/tromino_tiling.py
+++ assignment-2024-1/tromino_tiling.py
@@ -1,15 +1,18 @@
 import sys
 
 def calculateCenter(startX, endX, startY, endY):
-        centerX = int((startX + endX) / 2)
-        centerY = int((startY + endY) / 2)
-        return centerX, centerY
+    centerX = int((startX + endX) / 2)
+    centerY = int((startY + endY) / 2)
+    return centerX, centerY
 
-def fill(n, grid, startX, endX, startY, endY, missingX, missingY):
+def fill(n, grid, startX, endX, startY, endY, tromino_nums, missingX, missingY):
+
+    tromino_index = tromino_nums.pop(0)  #INDEX OF CURRENT TROMINO THAT WILL BE CREATED
+    tromino_nums.append(tromino_index)  #MOVES THE INDEX OF THIS TROMINO TO THE END SO THAT IT CAN NOT BE REUSED
 
     #GET CENTER OF THE CURRENT RECTANGLE AND SET THE MIDDLE COORDINATES
     #ul= Upper Left, ur= Upper Right, ll= Lower Left, lr= Lower Right
-
+    
     cX, cY = calculateCenter(startX, endX, startY, endY)
 
     ulX, ulY = cX, cY
@@ -22,30 +25,30 @@ def fill(n, grid, startX, endX, startY, endY, missingX, missingY):
 
     if missingX <= cX:  #LEFT
         if missingY <= cY: #LEFT AND UPPER BOUND
-            makeTromino(grid, cX, cY+1, cX+1, cY+1, cX+1, cY, n)
+            makeTromino(grid, cX, cY+1, cX+1, cY+1, cX+1, cY, tromino_index)
             ulX, ulY = missingX, missingY
         else: #LEFT AND LOWER BOUND
-            makeTromino(grid, cX, cY, cX+1, cY, cX+1, cY+1, n)
+            makeTromino(grid, cX, cY, cX+1, cY, cX+1, cY+1, tromino_index)
             llX, llY = missingX, missingY
     else: #RIGHT
         if missingY <= cY: #RIGHT AND UPPER BOUND
-            makeTromino(grid, cX, cY, cX, cY+1, cX+1, cY+1, n)            
+            makeTromino(grid, cX, cY, cX, cY+1, cX+1, cY+1, tromino_index)            
             urX, urY = missingX, missingY
         else: #RIGHT AND LOWER BOUND
-            makeTromino(grid, cX, cY, cX+1, cY, cX, cY+1, n)
+            makeTromino(grid, cX, cY, cX+1, cY, cX, cY+1, tromino_index)
             lrX, lrY = missingX, missingY
 
-    if n==1:
-        return
+    if n == 1:
+        return 
     else:
-        fill(n-1, grid, startX, cX, startY, cY, ulX, ulY) #RECURSION FOR UL
-        fill(n-1, grid, cX+1, endX, startY, cY, urX, urY) #RECURSION FOR UR
-        fill(n-1, grid, startX, cX, cY+1, endY, llX, llY) #RECURSION FOR LL
-        fill(n-1, grid, cX+1, endX, cY+1, endY, lrX, lrY) #RECURSION FOR LR
-    
+        fill(n-1, grid, startX, cX, startY, cY, tromino_nums, ulX, ulY) #RECURSION FOR UL
+        fill(n-1, grid, cX+1, endX, startY, cY, tromino_nums, urX, urY) #RECURSION FOR UR
+        fill(n-1, grid, startX, cX, cY+1, endY, tromino_nums, llX, llY) #RECURSION FOR LL
+        fill(n-1, grid, cX+1, endX, cY+1, endY, tromino_nums, lrX, lrY) #RECURSION FOR LR
+
 def placeTile(grid, x, y, n):
     grid[x][y] = n        
-    
+
 #PLACES TILES AROUND THE MISSING TILE - THE HOLE
 def makeTromino(grid, x1, y1, x2, y2, x3, y3, n):
     placeTile(grid, x1, y1, n)
@@ -56,20 +59,25 @@ def showGrid(grid):
     for j in range(len(grid)):
         for i in range(len(grid)):
             print(grid[i][j], end=" ")
-        print()
-    print()            
+        print()            
 
 def tileGrid(n): 
     size = 2**n
-    if n==1:
+    if n == 1:
         missingX = 1
         missingY = 0
     else: 
         missingX = 3
-        missingY = 3    
+        missingY = 3   
 
-    grid = [['X' if i == missingY and j == missingX else '0' for i in range(size)] for j in range(size)]
-    fill(n, grid, 0, size-1, 0, size-1, missingX, missingY)
+    #INITILIAZE GRID AND PLACE THE MISSING TILE / HOLE WITH AN 'X'
+    grid = [['X' if i == missingY and j == missingX else '0' for i in range(size)] for j in range(size)] 
+
+    #INITIALIZE THE LIST OF INDEXES FOR EACH TROMINO SO THAT IT CAN START COUNTING FROM 1
+    tromino_nums = list(range(1, (size * size) + 1))
+
+
+    fill(n, grid, 0, size-1, 0, size-1, tromino_nums, missingX, missingY)
     showGrid(grid)
 
 n = int(sys.argv[1])
