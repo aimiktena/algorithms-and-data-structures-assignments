d85ab657a4da519a4ad12d6075b5a8ae969c1fbf
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-04-23 00:08:35 +0300

    Add assign_color function to define each tromino's color and fill_colors function to fill the grid with the colors instead of the tromino's numbers

--- assignment-2024-1/tromino_tiling.py
+++ assignment-2024-1/tromino_tiling.py
@@ -5,7 +5,7 @@ def calculateCenter(startX, endX, startY, endY):
     centerY = int((startY + endY) / 2)
     return centerX, centerY
  
-# DEFINE THE 2 FOLLOWING FUNCTIONS IN ORDER TO CONVERT THE TILING PROBLEM TO A GRAPH COLORING PROBLEM (FOR THE COLORING OF THE TROMINOS)
+#CONVERT THE TILING PROBLEM TO A GRAPH COLORING PROBLEM (FOR THE COLORING OF THE TROMINOS)
 def are_adjacent(position1, position2):
     x1, y1 = position1
     x2, y2 = position2    
@@ -15,10 +15,8 @@ def assign_neighbours(tromino_positions):
     num_trominos = len(tromino_positions)
     # INITIALIZE LIST OF LISTS FOR STORING THE NEIGHBOURS
     neighbours = [[] for _ in range(num_trominos)]  
-
     for i in range(num_trominos):
-        for j in range(i + 1, num_trominos):  
-            is_neighbour = False
+        for j in range(i + 1, num_trominos):
             for position1 in tromino_positions[i]:
                 for position2 in tromino_positions[j]:
                     if are_adjacent(position1, position2):
@@ -30,14 +28,42 @@ def assign_neighbours(tromino_positions):
                 break
     return neighbours
 
-def fill(n, grid, startX, endX, startY, endY, tromino_nums, tromino_positions, missingX, missingY):
+def assign_color(neighbours):
+    green= set()
+    blue= set()
+    red= set()
+
+    green_restrictions = set()
+    blue_restrictions = set()
+    red_restrictions = set()
+
+    for i, neighbour_list in enumerate(neighbours):
+        if i not in green_restrictions:
+            green.add(i)
+            for neighbor in neighbour_list:
+                green_restrictions.add(neighbor)
+
+    for i, neighbour_list in enumerate(neighbours):
+        if i not in green and i not in blue_restrictions:
+            blue.add(i)
+            for neighbor in neighbour_list:
+                blue_restrictions.add(neighbor)
+
+    for i, neighbour_list in enumerate(neighbours):
+        if i not in green and i not in blue and i not in red_restrictions:
+            red.add(i)
+            for neighbor in neighbour_list:
+                red_restrictions.add(neighbor)
+
+    return green, blue, red
+
+def fill_numbers(n, grid, startX, endX, startY, endY, tromino_nums, tromino_positions, missingX, missingY):
     tromino_index = tromino_nums.pop(0)  # INDEX OF CURRENT TROMINO THAT WILL BE CREATED
     tromino_nums.append(tromino_index)  # MOVES THE INDEX OF THIS TROMINO TO THE END SO THAT IT CAN NOT BE REUSED
 
     # GET CENTER OF THE CURRENT RECTANGLE AND SET THE MIDDLE COORDINATES
     # ul= Upper Left, ur= Upper Right, ll= Lower Left, lr= Lower Right    
     cX, cY = calculateCenter(startX, endX, startY, endY)
-
     ulX, ulY = cX, cY
     urX, urY = cX+1, cY
     llX, llY = cX, cY+1
@@ -45,7 +71,6 @@ def fill(n, grid, startX, endX, startY, endY, tromino_nums, tromino_positions, m
 
     # CHECK IF THE MISSING TILE IS ON THE LEFT (OR OTHERWISE THE RIGHT) OF THE GRID
     # CHECK IF THE MISSING TILE IS ON THE UPPER (OR OTHERWISE THE LOWER) OF THE GRID
-
     if missingX <= cX:  # LEFT
         if missingY <= cY: # LEFT AND UPPER BOUND
             makeTromino(grid, cX, cY+1, cX+1, cY+1, cX+1, cY, tromino_index)
@@ -68,15 +93,24 @@ def fill(n, grid, startX, endX, startY, endY, tromino_nums, tromino_positions, m
     if n == 1:
         return 
     else:
-        fill(n-1, grid, startX, cX, startY, cY, tromino_nums, tromino_positions, ulX, ulY) # RECURSION FOR UL
-        fill(n-1, grid, cX+1, endX, startY, cY, tromino_nums, tromino_positions, urX, urY) # RECURSION FOR UR
-        fill(n-1, grid, startX, cX, cY+1, endY, tromino_nums, tromino_positions, llX, llY) # RECURSION FOR LL
-        fill(n-1, grid, cX+1, endX, cY+1, endY, tromino_nums, tromino_positions, lrX, lrY) # RECURSION FOR LR
+        fill_numbers(n-1, grid, startX, cX, startY, cY, tromino_nums, tromino_positions, ulX, ulY) # RECURSION FOR UL
+        fill_numbers(n-1, grid, cX+1, endX, startY, cY, tromino_nums, tromino_positions, urX, urY) # RECURSION FOR UR
+        fill_numbers(n-1, grid, startX, cX, cY+1, endY, tromino_nums, tromino_positions, llX, llY) # RECURSION FOR LL
+        fill_numbers(n-1, grid, cX+1, endX, cY+1, endY, tromino_nums, tromino_positions, lrX, lrY) # RECURSION FOR LR
+
+def fill_colors(grid, tromino_positions, green, blue, red):
+    for position_list in tromino_positions:
+        for x, y in position_list:
+            if tromino_positions.index(position_list) in green:
+                grid[x][y] = 'G'
+            elif tromino_positions.index(position_list) in blue:
+                grid[x][y] = 'B'
+            elif tromino_positions.index(position_list) in red:
+                grid[x][y] = 'R'
 
 def placeTile(grid, x, y, n):
     grid[x][y] = n        
-
-# PLACES TILES AROUND THE MISSING TILE - THE HOLE
+    
 def makeTromino(grid, x1, y1, x2, y2, x3, y3, n):
     placeTile(grid, x1, y1, n)
     placeTile(grid, x2, y2, n)
@@ -103,9 +137,12 @@ def tileGrid(n):
     tromino_nums = list(range(size * size))
     # INITILIAZE LIST TO STORE THE POSITIONS OF EACH TROMINO
     tromino_positions = []  
-
-    fill(n, grid, 0, size-1, 0, size-1, tromino_nums, tromino_positions, missingX, missingY)
-    showGrid(grid)
+    fill_numbers(n, grid, 0, size-1, 0, size-1, tromino_nums, tromino_positions, missingX, missingY)
     
+    neighbours = assign_neighbours(tromino_positions)    
+    green, blue, red = assign_color(neighbours)
+    fill_colors(grid, tromino_positions, green, blue, red)
+    showGrid(grid)
+
 n = int(sys.argv[1])
 tileGrid(n)
