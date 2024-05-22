8dc2deff5b9c6b6f0cf4907fe6dd48025e730b8a
Aimilia Dimitra Ktena <t8210073@aueb.gr>
2024-05-22 14:23:41 +0300

    Add two new math functions

--- assignment-2024-2/bursts.py
+++ assignment-2024-2/bursts.py
@@ -6,3 +6,12 @@ def calculateTransitionCost(i, j, gamma, n):
         return 0
     else: 
         return gamma * (j-i) * math.log(n)
+
+def exponential_distribution (x, lamda): 
+    if x>=0:
+        return lamda * math.exp(-lamda * x)
+    else:
+        return 0
+
+def calculateExpectedValue(lamda):
+    return 1/lamda
